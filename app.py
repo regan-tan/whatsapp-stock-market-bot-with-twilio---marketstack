2c7ead4b65b6e473d1a96259835f808e3e898fab
Regan Tan <regantanyk@gmail.com>
2025-03-12 15:24:45 +0800

    whatsapp chatbot works with twilio

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 from flask import Flask
 from flask import request
-
+from twilio.rest import Client
+import os
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return {
-        "Result": "You successfully created the first route!"
-    }
+ACCOUNT_ID = os.environ.get('TWILIO_ACCOUNT')
+TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+client = Client(ACCOUNT_ID, TWILIO_TOKEN)
+TWILIO_NUMBER = 'whatsapp:+14155238886'
+
+def process_msg(msg):
+    response = ""
+    if msg == "hi":
+        response = "Hello, welcome to the stock market chatbot!"
+    else: 
+        response = "Please type hi to get started"
+    return response
+
+def send_msg(msg, recipient):
+    client.messages.create(
+        from_=TWILIO_NUMBER,
+        body=msg,
+        to = recipient
+    )
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    message = request.form["message"]
+    f = request.form
+    msg = f['Body']
+    sender = f['From']
+    response = process_msg(msg)
+    send_msg(response, sender)
     return {
-        "Result": "message"
+        "OK", 200
     }
